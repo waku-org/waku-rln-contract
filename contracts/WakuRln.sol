d136499243962af8ee99bb359fb495d74d1b1c9e
alrevuelta <alvrevuelta@gmail.com>
2023-11-27 18:35:47 +0100

    poc

--- contracts/WakuRln.sol
+++ contracts/WakuRln.sol
@@ -5,16 +5,25 @@ import {IPoseidonHasher} from "rln-contract/PoseidonHasher.sol";
 import {RlnBase, DuplicateIdCommitment, FullTree, InvalidIdCommitment} from "rln-contract/RlnBase.sol";
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
+import {LazyIMT, LazyIMTData} from "@zk-kit/imt.sol/LazyIMT.sol";
+
 error NotImplemented();
 
 contract WakuRln is Ownable, RlnBase {
     uint16 public immutable contractIndex;
+
+    LazyIMTData tree;
+    using LazyIMT for LazyIMTData;
 
     constructor(
         address _poseidonHasher,
         uint16 _contractIndex
     ) Ownable() RlnBase(0, 20, _poseidonHasher, address(0)) {
         contractIndex = _contractIndex;
+
+        // TODO: This errors
+        uint8 merkleTreeDepth = 32;
+        tree.init(merkleTreeDepth);
     }
 
     /// Registers a member
@@ -24,6 +33,7 @@ contract WakuRln is Ownable, RlnBase {
 
         members[idCommitment] = idCommitmentIndex;
         memberExists[idCommitment] = true;
+        tree.insert(idCommitment);
 
         emit MemberRegistered(idCommitment, idCommitmentIndex);
         idCommitmentIndex += 1;
@@ -70,5 +80,21 @@ contract WakuRln is Ownable, RlnBase {
 
     function withdraw() external pure override {
         revert NotImplemented();
+    }
+
+    // TODO Some quick getter functions, unsure if needed
+    function merkleRoot() public view returns(uint256) {
+        return tree.root();
+    }
+
+    function numOfLeaves() public view returns(uint40) {
+        return tree.numberOfLeaves;
+    }
+
+    // Unsure how viable is to fetch elements one by one.
+    // Perhaps we just want the leafs and the client can reconstruct
+    // the hashes.
+    function getElement(uint256 elementIndex) public view returns(uint256) {
+        return tree.elements[elementIndex];
     }
 }
